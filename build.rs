7e87e29e1c3a43c74cff28a5d77ddcbee9178614
Martin Charles <martincharles07@gmail.com>
2018-12-05 20:20:33 -0500

    Static Instead of Dynamic

--- build.rs
+++ build.rs
@@ -3,5 +3,5 @@ extern crate cmake;
 fn main() {
     let dst = cmake::build("chromaprint");
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
-    println!("cargo:rustc-link-lib=chromaprint");
+    println!("cargo:rustc-link-lib=static=chromaprint");
 }
